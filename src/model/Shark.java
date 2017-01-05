a9e4b229363c7755d85cb26ebdfe513f1aad4774
Bastien Sebire <bastien.sebire@gmail.com>
2017-01-05 15:21:07 +0100

    Fish entities added

--- src/model/Shark.java
+++ src/model/Shark.java
@@ -1,5 +1,15 @@
 package model;
 
-public class Shark {
+public class Shark extends Fish{
+
+	public Shark(int age, boolean isDead, int pX, int pY) {
+		super(age, isDead, pX, pY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Shark []";
+	}
 
 }
